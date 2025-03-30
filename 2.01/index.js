73e781d4df6ba327fc69957877339e0356d5499d
Ananya Mohapatra <ananyamohapatra0102@gmail.com>
2025-03-30 23:44:10 +0530

    Integrating body-parser library.

---
+++ 2.01/index.js
@@ -0,0 +1,33 @@
+const express = require("express")
+
+const app = express()
+
+const port = 8000
+
+function calcSum(count){
+    sum = 0
+
+    for(let i = 0;i< count;i++){
+        sum = sum +i
+    }
+    return sum
+}
+
+
+function handSum(req,res){
+    let finalSum = calcSum(1000)
+    console.log(finalSum);
+
+    let answer = `The calculated sum is ${finalSum}` 
+
+     res.send(answer)
+}
+
+
+app.get('/handleSum',handSum)
+
+function start(){
+    console.log(`Example app listening port  ${port}`);
+}
+
+app.listen(port,start) 
