eb8badc8d9e3645cc6b5d55199490b44a6dbb4fc
Ananya Mohapatra <ananyamohapatra0102@gmail.com>
2025-05-20 23:21:12 +0530

    Sending quer from the node js processes to the http server.

--- 2.07/index.js
+++ 2.07/index.js
@@ -1,2 +1,36 @@
-const express = require('express')
+const express = require("express")
+
 const app = express()
+
+const port = 3005
+
+function calcSum(count){
+    sum = 0
+
+    for(let i = 0;i< count;i++){
+        sum = sum +i
+    }
+    return sum
+}
+
+
+function handSum(req,res){
+    const count = req.query.count
+    let finalSum = calcSum(count)
+    //console.log(finalSum);
+      
+    let response ={
+        sum
+    } 
+
+     res.send(response)
+}
+
+
+app.get('/Sum',handSum)
+
+function start(){
+    console.log(`Example app listening port  ${port}`);
+}
+
+app.listen(port,start) 
